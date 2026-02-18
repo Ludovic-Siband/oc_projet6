691fcecfc7e9d65b2d3dc1a675253c433bd98f38
bancs6 <ludovic.siband@gmail.com>
2026-02-18 17:53:17 +0100

    docs(front): add JSDoc to olympic service

--- front/src/app/core/services/olympic.service.ts
+++ front/src/app/core/services/olympic.service.ts
@@ -8,10 +8,16 @@ import { Olympic } from '../models/Olympic';
   providedIn: 'root',
 })
 export class OlympicService {
+  /**
+   * JSON mock data location for local use.
+   */
   private olympicUrl = './assets/mock/olympic.json';
 
   constructor(private http: HttpClient) {}
 
+  /**
+   * Fetch the list of olympic entries from the mock dataset.
+   */
   getOlympics(): Observable<Array<Olympic>> {
     return this.http.get<Array<Olympic>>(this.olympicUrl).pipe(
       catchError((error, caught) => {
